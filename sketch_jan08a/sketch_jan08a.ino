1d93925b0de572bd77c78714c94b0794ec16e630
Halima Koundi <hkoundi@gmail.com>
2019-01-20 17:11:58 +0000

    sketches for the spinning thingy - arduino ide

---
+++ sketch_jan08a/sketch_jan08a.ino
@@ -0,0 +1,54 @@
+// Simple strand test for Adafruit Dot Star RGB LED strip.
+// This is a basic diagnostic tool, NOT a graphics demo...helps confirm
+// correct wiring and tests each pixel's ability to display red, green
+// and blue and to forward data down the line.  By limiting the number
+// and color of LEDs, it's reasonably safe to power a couple meters off
+// the Arduino's 5V pin.  DON'T try that with other code!
+
+#include <Adafruit_DotStar.h>
+// Because conditional #includes don't work w/Arduino sketches...
+#include <SPI.h>         // COMMENT OUT THIS LINE FOR GEMMA OR TRINKET
+//#include <avr/power.h> // ENABLE THIS LINE FOR GEMMA OR TRINKET
+
+#define NUMPIXELS 72 // Number of LEDs in strip
+
+// Here's how to control the LEDs from any two pins:
+  #define DATAPIN    6 //A1 //green 
+  #define CLOCKPIN   1 //A7 //blue
+Adafruit_DotStar strip = Adafruit_DotStar(
+  NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
+// The last parameter is optional -- this is the color data order of the
+// DotStar strip, which has changed over time in different production runs.
+// Your code just uses R,G,B colors, the library then reassigns as needed.
+// Default is DOTSTAR_BRG, so change this if you have an earlier strip.
+
+// Hardware SPI is a little faster, but must be wired to specific pins
+// (Arduino Uno = pin 11 for data, 13 for clock, other boards are different).
+//Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS, DOTSTAR_BRG);
+
+void setup() {
+
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000L)
+  clock_prescale_set(clock_div_1); // Enable 16 MHz on Trinket
+#endif
+
+  strip.begin(); // Initialize pins for output
+  strip.show();  // Turn all LEDs off ASAP
+}
+
+void loop() {
+  //byte * colors = getColurs();
+//  Serial.println(colors[1]);
+  for(int i = 0; i < NUMPIXELS; i++){
+      strip.setPixelColor(i, 0xFF0000);
+     // Serial.println(colors[i]);
+  }
+  strip.show(); 
+  delay(100);
+}
+
+//byte* getColurs(){
+//
+//  byte firstRow[NUMPIXELS]={0x001100,0x001100,0x001100};
+//  return firstRow;
+//}
