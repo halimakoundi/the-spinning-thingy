cc617071aab4caea192c8c88f31bdf0c490193f5
Dan Cohen <dan@devshell.co.uk>
2019-05-02 22:46:06 +0100

    add calibration

---
+++ calibration/calibration.ino
@@ -0,0 +1,35 @@
+//#define NUMPIXELS 72 // Number of LEDs in strip
+//
+//#include <Adafruit_DotStar.h>
+//// Because conditional #includes don't work w/Arduino sketches...
+//#include <SPI.h>         // COMMENT OUT THIS LINE FOR GEMMA OR TRINKET
+//
+//  #define DATAPIN    6 //A1 //green 
+//  #define CLOCKPIN   1 //A7 //blue
+//Adafruit_DotStar strip = Adafruit_DotStar(
+//  NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
+//int start = 0;
+//void setup() {
+//
+//#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000L)
+//  clock_prescale_set(clock_div_1); // Enable 16 MHz on Trinket
+//#endif
+//  strip.begin(); // Initialize pins for output
+//  strip.show();  // Turn all LEDs off ASAP
+//
+//  uint32_t white = 0xFF0000;
+//
+//  for(int i = 0; i < NUMPIXELS; i++){
+//      strip.setPixelColor(i, white);
+//  }
+//  strip.show();
+//}
+//
+//void loop() {
+//}
+
+void setup() {
+}
+
+void loop() {
+}
